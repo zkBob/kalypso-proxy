b08e582870d15c9749f7663fd848a28ee07cc339
vladimir <vladimir@blockscout.com>
2024-05-31 14:20:28 +0300

    init

---
+++ index.ts
@@ -0,0 +1,91 @@
+import express from "express";
+import { ethers } from "ethers";
+import { KalypsoSdk } from "kalypso-sdk";
+import { KalspsoConfig } from "kalypso-sdk/dist/types";
+import { readFileSync } from "fs";
+import BigNumber from "bignumber.js";
+import cors from "cors";
+const app = express();
+const rpc = "https://arbitrum-sepolia.blockpi.network/v1/rpc/public";
+
+const provider = new ethers.JsonRpcProvider(rpc);
+app.use(cors());
+app.use(express.json());
+const sk: string = process.env.SK ?? "CHANGE_ME";
+const wallet = new ethers.Wallet(sk, provider);
+
+const kalypsoConfig: KalspsoConfig = JSON.parse(
+  readFileSync("kalypso-config.json", "utf-8"),
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
+app.get("/version", (req, res) => {
+  res.send({ ref: "0.1.0", commitHash: "0abcd" });
+});
+app.post("/proveTx", async (req, res) => {
+  const reward = "1000000000000000000";
+
+  const kalypso = new KalypsoSdk(wallet, kalypsoConfig);
+
+  const latestBlock = await provider.getBlockNumber();
+
+  const body = req.body;
+
+  const secretInputs = body.secretInputs;
+  console.log;
+  let abiCoder = new ethers.AbiCoder();
+  let inputBytes = abiCoder.encode(
+    ["uint256[5]"],
+    [[body.root, body.nullifier, body.out_commit, body.delta, body.memo]],
+  );
+  const marketId =
+    "0x07b7d625c70be57115ab18fc435ed0253425671cb91bd6547b7defbc75f52082";
+  const assignmentDeadline = new BigNumber(latestBlock).plus(10000000000);
+  console.log({
+    latestBlock,
+    assignmentDeadline: assignmentDeadline.toFixed(0),
+  });
+  const proofGenerationTimeInBlocks = new BigNumber(10000000000);
+
+  // Create ASK request
+  try {
+    const askRequest = await kalypso.MarketPlace().createAsk(
+      marketId,
+      inputBytes,
+      reward,
+      assignmentDeadline.toFixed(0),
+      proofGenerationTimeInBlocks.toFixed(0),
+      await wallet.getAddress(),
+      0, // TODO: keep this 0 for now
+      Buffer.from(secretInputs),
+    );
+    await askRequest.wait();
+    console.log("Ask Request Hash: ", askRequest.hash);
+    res.send(askRequest.hash);
+  } catch (e) {
+    console.log("exception :", e);
+    res.status(500).send(e);
+  }
+});
+
+app.listen(8081, async () => {
+  console.log("listening to 8081");
+  const address = await wallet.getAddress();
+  console.log(`using wallet ${address}`);
+  const walletBalanceString = (await provider.getBalance(address)).toString();
+  const decimals = 18;
+
+  const balanceStringWithDecimalPoint =
+    walletBalanceString.length < 18
+      ? ["0.", walletBalanceString.padStart(decimals, "0")].join("")
+      : [
+          walletBalanceString.slice(0, walletBalanceString.length - decimals),
+          ".",
+          walletBalanceString.slice(walletBalanceString.length - decimals),
+        ].join("");
+  console.log(
+    `current balance is  ${Number.parseFloat(balanceStringWithDecimalPoint)}`,
+  );
+});
